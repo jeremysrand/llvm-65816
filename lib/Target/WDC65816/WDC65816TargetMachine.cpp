036df005afaf69788f4b6432faf4ad43ac6dd57d
Jeremy Rand <jeremy@rand-family.com>
2014-07-06 23:24:07 -0400

    Add some more boilerplate for the 65816.

---
+++ lib/Target/WDC65816/WDC65816TargetMachine.cpp
@@ -0,0 +1,71 @@
+//===-- WDC65816TargetMachine.cpp - Define TargetMachine for WDC65816 -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "WDC65816TargetMachine.h"
+#include "WDC65816.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/PassManager.h"
+#include "llvm/Support/TargetRegistry.h"
+
+using namespace llvm;
+
+extern "C" void LLVMInitializeWDC65816Target() {
+    // Register the target.
+    RegisterTargetMachine<WDC65816TargetMachine> X(TheWDC65816Target);
+}
+
+/// WDC65816TargetMachine ctor
+///
+WDC65816TargetMachine::WDC65816TargetMachine(const Target &T, StringRef TT,
+                                       StringRef CPU, StringRef FS,
+                                       const TargetOptions &Options,
+                                       Reloc::Model RM, CodeModel::Model CM,
+                                       CodeGenOpt::Level OL)
+: LLVMTargetMachine(T, TT, CPU, FS, Options, RM, CM, OL),
+DL(Subtarget.getDataLayout()),
+InstrInfo(Subtarget),
+TLInfo(*this), TSInfo(*this),
+FrameLowering(Subtarget) {
+    initAsmInfo();
+}
+
+namespace {
+    /// WDC65816 Code Generator Pass Configuration Options.
+    class WDC65816PassConfig : public TargetPassConfig {
+    public:
+        WDC65816PassConfig(WDC65816TargetMachine *TM, PassManagerBase &PM)
+        : TargetPassConfig(TM, PM) {}
+        
+        WDC65816TargetMachine &getWDC65816TargetMachine() const {
+            return getTM<WDC65816TargetMachine>();
+        }
+        
+        virtual bool addInstSelector();
+        virtual bool addPreEmitPass();
+    };
+} // namespace
+
+TargetPassConfig *WDC65816TargetMachine::createPassConfig(PassManagerBase &PM) {
+    return new WDC65816PassConfig(this, PM);
+}
+
+bool WDC65816PassConfig::addInstSelector() {
+    addPass(createWDC65816ISelDag(getWDC65816TargetMachine()));
+    return false;
+}
+
+/// addPreEmitPass - This pass may be implemented by targets that want to run
+/// passes immediately before machine code is emitted.  This should return
+/// true if -print-machineinstrs should print out the code after the passes.
+bool WDC65816PassConfig::addPreEmitPass(){
+    return true;
+}
---
+++ lib/Target/WDC65816/WDC65816TargetMachine.h
@@ -0,0 +1,62 @@
+//===-- WDC65816TargetMachine.h - Define TargetMachine for WDC65816 ---*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the Sparc specific subclass of TargetMachine.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef WDC65816TARGETMACHINE_H
+#define WDC65816TARGETMACHINE_H
+
+#include "WDC65816FrameLowering.h"
+#include "WDC65816ISelLowering.h"
+#include "WDC65816InstrInfo.h"
+#include "WDC65816SelectionDAGInfo.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/Target/TargetFrameLowering.h"
+#include "llvm/Target/TargetMachine.h"
+
+namespace llvm {
+    
+    class WDC65816TargetMachine : public LLVMTargetMachine {
+        // SparcSubtarget Subtarget;   JSR_TODO - Do I need this?
+        const DataLayout DL;       // Calculates type size & alignment
+        WDC65816InstrInfo InstrInfo;
+        WDC65816TargetLowering TLInfo;
+        WDC65816SelectionDAGInfo TSInfo;
+        WDC65816FrameLowering FrameLowering;
+    public:
+        WDC65816TargetMachine(const Target &T, StringRef TT,
+                           StringRef CPU, StringRef FS, const TargetOptions &Options,
+                           Reloc::Model RM, CodeModel::Model CM,
+                           CodeGenOpt::Level OL);
+        
+        virtual const WDC65816InstrInfo *getInstrInfo() const { return &InstrInfo; }
+        virtual const TargetFrameLowering  *getFrameLowering() const {
+            return &FrameLowering;
+        }
+        virtual const WDC65816RegisterInfo *getRegisterInfo() const {
+            return &InstrInfo.getRegisterInfo();
+        }
+        virtual const WDC65816TargetLowering* getTargetLowering() const {
+            return &TLInfo;
+        }
+        virtual const WDC65816SelectionDAGInfo* getSelectionDAGInfo() const {
+            return &TSInfo;
+        }
+        virtual const DataLayout       *getDataLayout() const { return &DL; }
+        
+        // Pass Pipeline Configuration
+        virtual TargetPassConfig *createPassConfig(PassManagerBase &PM);
+        virtual bool addCodeEmitter(PassManagerBase &PM, JITCodeEmitter &JCE);
+    };
+    
+} // end namespace llvm
+
+#endif
